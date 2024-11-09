8c617318578a9fab6faa3ce8b6ea3c1b80c36f13
ThatGirl03 <kwaneleayandamazibuko@gmail.com>
2024-11-09 04:10:50 +0200

    update on config

--- website/firebase_config.py
+++ website/firebase_config.py
@@ -1,10 +1,16 @@
 # website/firebase_config.py
-import firebase_admin
-from firebase_admin import credentials, firestore, auth
+import json
+import os
 
+from firebase_admin import credentials, firestore, initialize_app
+
+
+
+firebase_key_json = os.getenv('FIREBASE_KEY')
+firebase_key_dict = json.loads(firebase_key_json)
 # Initialize Firebase using the Firebase Admin SDK private key JSON file
-cred = credentials.Certificate("firebase_key.json")  # Update this path as needed
-firebase_admin.initialize_app(cred)
+cred = credentials.Certificate("firebase_key_dict")  # Update this path as needed
+initialize_app(cred)
 
 # Set up Firestore client
 db = firestore.client()
