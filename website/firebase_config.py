ac400734bde04140830d7c9a3bf2a25602586dc9
ThatGirl03 <kwaneleayandamazibuko@gmail.com>
2024-11-09 04:43:33 +0200

    kh

--- website/firebase_config.py
+++ website/firebase_config.py
@@ -2,17 +2,18 @@ import json
 import os
 from firebase_admin import credentials, firestore, initialize_app
 
-# Load Firebase key from environment variable
+# Get Firebase key from environment variable
 firebase_key_json = os.getenv('FIREBASE_KEY')
+
 if not firebase_key_json:
     raise Exception("FIREBASE_KEY environment variable is not set.")
 
-try:
-    # Convert JSON string to dictionary and initialize Firebase
-    firebase_key_dict = json.loads(firebase_key_json)
-    cred = credentials.Certificate(firebase_key_dict)
-    initialize_app(cred)
-    
-    db = firestore.client()
-except json.JSONDecodeError:
-    raise Exception("FIREBASE_KEY environment variable is not a valid JSON string.")
+# Parse the Firebase key JSON string to a Python dictionary
+firebase_key_dict = json.loads(firebase_key_json)
+
+# Initialize Firebase using the Firebase Admin SDK private key JSON dictionary
+cred = credentials.Certificate(firebase_key_dict)
+initialize_app(cred)
+
+# Set up Firestore client
+db = firestore.client()
