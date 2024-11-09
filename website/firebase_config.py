50a8238a530131ead34a33d9523e25aeb6e6259b
ThatGirl03 <kwaneleayandamazibuko@gmail.com>
2024-11-09 04:12:05 +0200

    Update for error

--- website/firebase_config.py
+++ website/firebase_config.py
@@ -8,6 +8,8 @@ from firebase_admin import credentials, firestore, initialize_app
 
 firebase_key_json = os.getenv('FIREBASE_KEY')
 firebase_key_dict = json.loads(firebase_key_json)
+if not firebase_key_json:
+    raise Exception("FIREBASE_KEY environment variable is not set.")
 # Initialize Firebase using the Firebase Admin SDK private key JSON file
 cred = credentials.Certificate("firebase_key_dict")  # Update this path as needed
 initialize_app(cred)
